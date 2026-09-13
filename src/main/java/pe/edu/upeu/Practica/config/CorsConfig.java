45042e9bf1c81b6d4074af54024e399e6b081328
Esther Landa <esther07landatorres@gmail.com>
2026-09-13 17:26:05 -0500

    v1.1.1

--- src/main/java/pe/edu/upeu/Practica/config/CorsConfig.java
+++ src/main/java/pe/edu/upeu/Practica/config/CorsConfig.java
@@ -1,18 +1,23 @@
 package pe.edu.upeu.Practica.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
